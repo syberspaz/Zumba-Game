e93fe393d49d77ef3940d02f189cf15cf9e162bd
alberttransuushi <98855552+alberttransuushi@users.noreply.github.com>
2023-05-16 22:22:46 -0400

    Adding Kinect Hand 2D to thing

--- Assets/Scripts/Matching Card Game/FlipCard.cs
+++ Assets/Scripts/Matching Card Game/FlipCard.cs
@@ -6,32 +6,60 @@ using TMPro;
 using UnityEngine.EventSystems;
 
 public class FlipCard : MonoBehaviour, IPointerClickHandler {
-  public CardManager cardManager;
-  public GameObject back;
-  public bool isFront;
-  public bool locked = false;
-  // Start is called before the first frame update
-  void Start() {
-    back = transform.GetChild(0).gameObject;
-    isFront = false;
-    locked = false;
-  }
+    public CardManager cardManager;
+    public GameObject back;
+    public bool isFront;
+    public bool locked = false;
+    public bool isSelected = false;
+    public KinectHand2D kHand;
+    RectTransform rectTransform;
+    // Start is called before the first frame update
 
-  void Update() {
-    if (isFront) {
-      back.SetActive(false);
-    } else if (!isFront) {
-      back.SetActive(true);
+    void Start() {
+        back = transform.GetChild(0).gameObject;
+        isFront = false;
+        locked = false;
+        rectTransform = GetComponent<RectTransform>();
+
     }
 
-  }
+    void Update()
+    {
+        float dist = Vector2.Distance(kHand.rectTransform.localPosition, rectTransform.localPosition);
 
-  public void OnPointerClick(PointerEventData pointerEventData) {
-    if (pointerEventData.button == PointerEventData.InputButton.Left) {
-      if (!locked && !isFront) {
-        cardManager.FlipCard(this.gameObject);
-        isFront = true;
-      }
+        if (isFront)
+        {
+            back.SetActive(false);
+        }
+        else if (!isFront)
+        {
+            back.SetActive(true);
+        }
+        if (dist < rectTransform.rect.width / 2f)
+        {
+            // kHand.updateSelection();
+            if (kHand.handIsClosed && !isFront && !locked)
+            {
+                cardManager.FlipCard(this.gameObject);
+                isFront = true;
+                isSelected = true;
+            }
+
+
+        }
     }
-  }
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+
+            if (pointerEventData.button == PointerEventData.InputButton.Left)
+            {
+                if (!locked && !isFront)
+                {
+                    cardManager.FlipCard(this.gameObject);
+                    isFront = true;
+                }
+
+            }
+        }
 }
+
