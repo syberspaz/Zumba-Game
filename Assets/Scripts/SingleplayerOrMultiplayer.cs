6adaa1e65fa7ef9fa34efb8c96708238ec2a910b
Farhan <syedfarhan.hussain@ontariotechu.net>
2025-03-19 22:23:08 -0400

    Updated Menu

---
+++ Assets/Scripts/SingleplayerOrMultiplayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class SingleplayerOrMultiplayer : MonoBehaviour
+{
+    public GameObject objectToActivate;
+
+    public void SetBoolTrue()
+    {
+      
+        objectToActivate.SetActive(true);
+    }
+
+}
