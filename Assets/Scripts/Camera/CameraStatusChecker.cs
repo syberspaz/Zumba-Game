1b72fc4152fc969754f2c0fc928a02332efb99db
Sunu <sunupiousjoseph@gmail.com>
2025-03-31 23:50:30 -0400

    Camera Lag fix with Status Checker

--- Assets/Scripts/Camera/CameraStatusChecker.cs
+++ Assets/Scripts/Camera/CameraStatusChecker.cs
@@ -9,21 +9,23 @@ public class CameraStatusChecker : MonoBehaviour
     public UnityEngine.UI.Image statusIndicator; // UI Image to show connection status
     public Color connectedColor = Color.green; // Green when connected
     public Color disconnectedColor = Color.red; // Red when disconnected
+    
 
     private void Start()
     {
-        CheckCameraStatus();
+        UpdateUI(SkeletalTrackingProvider.IsKinectConnected);
+        //CheckCameraStatus();
     }
 
     private void Update()
     {
-        CheckCameraStatus();
+        UpdateUI(SkeletalTrackingProvider.IsKinectConnected);
+        //CheckCameraStatus(); 
     }
 
     private void CheckCameraStatus()
     {
         bool isCameraConnected = false;
-
         try
         {
             // Attempt to open the first available device
@@ -41,14 +43,32 @@ public class CameraStatusChecker : MonoBehaviour
         if (statusIndicator != null)
         {
             if (isCameraConnected)
-            {
+            { 
+                //this.gameObject.SetActive(false);
                 statusIndicator.color = connectedColor; // Set to green if the camera is connected
+                          
             }
             else
             {
+                //this.gameObject.SetActive(true);
+                //isConnected = true;
                 statusIndicator.color = disconnectedColor; // Set to red if the camera is not connected
             }
         }
     }
+
+    private void UpdateUI(bool isConnected)
+    {
+        if (isConnected)
+        {
+            statusIndicator.color = connectedColor; // Set to green if the camera is connected
+
+        }
+        else
+        {
+            statusIndicator.color = disconnectedColor; // Set to red if the camera is not connected
+        }
+    }
+
 
 }
